748b4de3e461534cf2dfd2c52d45bd807f459ed2
Zach Macke <zachmacke@gmail.com>
2020-02-28 17:11:57 -0600

    added to scroll into view

--- src/App.js
+++ src/App.js
@@ -5,15 +5,18 @@ import HomePage from './pages/HomePage'
 import ExperiencePage from './pages/ExperiencePage'
 import ProjectPage from './pages/ProjectPage'
 import AboutPage from './pages/AboutPage'
+import ScrollIntoView from "../src/components/ScrollIntoView"
 
 export default function App() {
   return (
-    <Switch>
-      <Route exact path="/" component={HomePage} />
-      <Route exact path="/experience" component={ExperiencePage} />
-      <Route exact path="/experience" component={ExperiencePage} />
-      <Route exact path="/projects" component={ProjectPage} />
-      <Route exact path="/about" component={AboutPage} />
-    </Switch>
+    <ScrollIntoView>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/experience" component={ExperiencePage} />
+        <Route exact path="/experience" component={ExperiencePage} />
+        <Route exact path="/projects" component={ProjectPage} />
+        <Route exact path="/about" component={AboutPage} />
+      </Switch>
+    </ScrollIntoView>
   )
 }
